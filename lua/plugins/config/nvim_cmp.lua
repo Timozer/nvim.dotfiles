5630ed4eca068a8077cc07f3cf0ba5cd611edc22
Timozer <zhenyuwang94@gmail.com>
2023-06-17 15:00:02 +0000

    nvm_cmp view change selection_order to near_cursor, nvm_cmp add cmp menu hls

--- lua/plugins/config/nvim_cmp.lua
+++ lua/plugins/config/nvim_cmp.lua
@@ -77,7 +77,10 @@ function M.config()
                 luasnip = "[Snip]",
                 path = "[Path]",
             })}),
-        }
+        },
+        view = {                                                        
+            entries = {name = 'custom', selection_order = 'near_cursor' } 
+        },    
     })
 
     -- for spell
@@ -115,6 +118,23 @@ function M.config()
     -- If you want insert `(` after select function or method item
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
+    -- gray
+    vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg='NONE', strikethrough=true, fg='#808080' })
+    -- blue
+    vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg='NONE', fg='#569CD6' })
+    vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link='CmpIntemAbbrMatch' })
+    -- light blue
+    vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { bg='NONE', fg='#9CDCFE' })
+    vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { link='CmpItemKindVariable' })
+    vim.api.nvim_set_hl(0, 'CmpItemKindText', { link='CmpItemKindVariable' })
+    -- pink
+    vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { bg='NONE', fg='#C586C0' })
+    vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { link='CmpItemKindFunction' })
+    -- front
+    vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg='NONE', fg='#D4D4D4' })
+    vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link='CmpItemKindKeyword' })
+    vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link='CmpItemKindKeyword' })
 end
 
 return M
